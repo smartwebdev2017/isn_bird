b0dc51eaf28b1bedc7e032e03736e767aceef09d
smart <smartwebdev2017@gmail.com>
2020-04-09 22:08:19 -0400

    update

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import sys
 import os
 import requests
 import json
-
+from flask import Flask, request, jsonify
 
 sys.path.append('/var/www')
 
@@ -12,6 +12,8 @@ BASE_URL = 'https://goisn.net/inspectup/rest'
 USER_NAME = 'Jana'
 USER_PASSWORD = 'ok4u2:Login'
 COMPANY_KEY = 'inspectup'
+BID = '156944290450716'
+API_KEY = '5tEmf4QlEM0rpRiyGqxHxHGtYqa1nehG'
 
 app = Flask(__name__)
 
@@ -19,64 +21,75 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/post", methods = ['POST'])
+@app.route("/post", methods = ['GET', 'POST'])
 def post():
+    id = request.values['order_id']
 
-    # #if request.method =='POST':
-    # params = {
-    #     'after': '04/01/2020',
-    #     # 'end': '2020-04-09T14:03:01.956Z'
-    # }
-    # res = requests.get(BASE_URL + '/orders', auth=(USER_NAME, USER_PASSWORD), params=params)
-    # orders = json.loads(res.text)
-    #
-    # for order in orders['orders']:
-    #     res = requests.get(BASE_URL + '/order/{}'.format(order['id']), auth=(USER_NAME, USER_PASSWORD))
-    #     detailed_order = json.loads(res.text)
-    #     client_id = detailed_order['order']['client']
-    #
-    #     res = requests.get(BASE_URL + '/client/{}'.format(client_id), auth=(USER_NAME, USER_PASSWORD))
-    #     client_data = json.loads(res.text)
-    #
-    #     first_name = client_data['client']['first']
-    #     last_name = client_data['client']['last']
-    #     email = client_data['client']['email']
-    #
-    #     mobile = client_data['client']['mobilephone']
-    #
-    #
-    # params = {
-    #     'after': '04/01/2020',
-    #     #'end': '2020-04-09T14:03:01.956Z'
-    # }
-    # res = requests.get(BASE_URL + '/orders' , auth=(USER_NAME, USER_PASSWORD), params=params)
-    # orders = json.loads(res._content)
-    #
-    # for order in orders['orders']:
-    #     res = requests.get(BASE_URL + '/order/{}'.format(order['id']), auth=(USER_NAME, USER_PASSWORD))
-    #     detailed_order = json.loads(res._content)
-    #     pass
-    # print(res)
-    #
-    # return render_template('index.html')
-    res = request.get_json()
-    print(res)
-    return request.get_json()
+    res = requests.get(BASE_URL + '/order/{}'.format(id), auth=(USER_NAME, USER_PASSWORD))
+    detailed_order = json.loads(res.text)
+    try:
+        client_id = detailed_order['order']['client']
+
+        res = requests.get(BASE_URL + '/client/{}'.format(client_id), auth=(USER_NAME, USER_PASSWORD))
+        client_data = json.loads(res.text)
+
+        first_name = client_data['client']['first']
+        last_name = client_data['client']['last']
+        email = client_data['client']['email']
+
+        mobile = client_data['client']['mobilephone']
+
+        first_name = 'test'
+        last_name = ''
+        email = 'test@test.com'
+        mobile = '408-xxx-xxxx'
+
+        values = '{"name": "' + first_name + ' ' + last_name + '", "emailId": "' + email + '", "phone": "' + mobile + '", "smsEnabled": 1}'
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+        request = requests.post(
+            'https://api.birdeye.com/resources/v1/customer/checkin?bid={}&api_key={}'.format(BID, API_KEY), data=values,
+            headers=headers)
+
+    except Exception as e:
+        pass
+
+
+    return render_template('pdf.html')
 
 @app.route("/order/<id>", methods=['GET'])
 def order(id):
     res = requests.get(BASE_URL + '/order/{}'.format(id), auth=(USER_NAME, USER_PASSWORD))
     detailed_order = json.loads(res.text)
-    client_id = detailed_order['order']['client']
+    try:
+        client_id = detailed_order['order']['client']
 
-    res = requests.get(BASE_URL + '/client/{}'.format(client_id), auth=(USER_NAME, USER_PASSWORD))
-    client_data = json.loads(res.text)
+        res = requests.get(BASE_URL + '/client/{}'.format(client_id), auth=(USER_NAME, USER_PASSWORD))
+        client_data = json.loads(res.text)
 
-    first_name = client_data['client']['first']
-    last_name = client_data['client']['last']
-    email = client_data['client']['email']
+        first_name = client_data['client']['first']
+        last_name = client_data['client']['last']
+        email = client_data['client']['email']
 
-    mobile = client_data['client']['mobilephone']
+        mobile = client_data['client']['mobilephone']
+    except Exception as e:
+        pass
+
+    first_name = 'test'
+    last_name = ''
+    email = 'test@test.com'
+    mobile = '408-xxx-xxxx'
+
+    values = '{"name": "' + first_name + ' ' + last_name + '", "emailId": "' + email + '", "phone": "' + mobile + '", "smsEnabled": 1}'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    request = requests.post('https://api.birdeye.com/resources/v1/customer/checkin?bid={}&api_key={}'.format(BID, API_KEY), data=values, headers=headers)
 
     return render_template('pdf.html')
 
